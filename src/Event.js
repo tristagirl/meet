fbad74ba75feff3ab544644e4a7d275a6d128aa0
Trista <tdahleen@me.com>
2022-04-07 16:03:51 -0500

    added tests and passed

---
+++ src/Event.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class Event extends Component {
+
+state = {
+    collapsed: true
+}
+
+handleClick = () => {
+    this.setState({
+        collapsed: !this.state.collapsed
+    });
+};
+
+  render() {
+    const { event } = this.props;
+    const { collapsed } = this.state;
+
+    return <div className="event">
+        <h2 className="summary">{event.summary}</h2>
+        <p className="start-date">
+            {event.start.dateTime} ({event.start.timeZone})
+        </p>
+        <p className="location">
+            {event.location}
+        </p>
+        <button className={`${collapsed ? "show" : "hide"}-details`} onClick={this.handleClick}>{collapsed ? "Show Details" : "Hide Details"}</button>
+    </div>;
+  }
+}
+export default Event;
