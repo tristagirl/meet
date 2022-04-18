4d15b7b876caa60d1cfa66c0c8e80d9c34703982
Trista <tdahleen@me.com>
2022-04-18 15:57:17 -0500

    4.8

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import "./nprogress.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
+
 import NumberOfEvents from "./NumberOfEvents";
-import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
-import "./nprogress.css";
+import { getEvents } from "./api";
+import { OfflineAlert } from "./Alert";
+import moment from "moment";
 import {
   ScatterChart,
   Scatter,
@@ -14,116 +17,123 @@ import {
   Tooltip,
   ResponsiveContainer,
 } from "recharts";
-// import { StatusAlert } from "./Alert";
-import WelcomeScreen from "./WelcomeScreen";
-
 
 class App extends Component {
+  componentDidMount() {
+    getEvents().then((response) => {
+      this.setState({ events: response.events, locations: response.locations });
+    });
+    //window.addEventListener("online", this.offLineAlert());
+  }
+
   state = {
     events: [],
-    locations: [],
+    page: null,
+    currentLocation: "all",
+    offlineText: "",
     numberOfEvents: 32,
-    showWelcomeScreen: undefined,
+    locations: [],
+  };
+
+  /*   offLineAlert = () => {
+    if (navigator.onLine === false) {
+      this.setState({
+        offlineText:
+          "You appear to be offline, this list is cached. Please connect to the internet for an updated list.",
+      });
+    } else {
+      this.setState({
+        offlineText: "",
+      });
+    }
+  };
+ */
+
+  formatDate = (date) => moment(date).format("YYYY-MM-DD HH:mm");
+
+  countEventsOnADate = (date) => {
+    const count = this.state.events.filter(
+      (event) => this.formatDate(event.start.dateTime) === date
+    );
+    return count.length;
   };
 
   getData = () => {
-    const { locations, events } = this.state;
-    const data = locations.map((location) => {
-      const number = events.filter(
-        (event) => event.location === location
-      ).length;
-      const city = location.split(", ").shift();
-      return { city, number };
+    const currentDate = moment().add(7, "d").format("YYYY-MM-DD HH:mm");
+
+    const next7Days = this.state.events.filter((event) => {
+      const eventDate = moment(event.start.dateTime).format("YYYY-MM-DD HH:mm");
+      return eventDate <= currentDate;
     });
+
+    const data = next7Days.map((event) => {
+      const date = this.formatDate(event.start.dateTime);
+      return { date, number: this.countEventsOnADate(date) };
+    });
+
     return data;
   };
 
-  async componentDidMount() {
-    this.mounted = true;
 
-    const accessToken = localStorage.getItem("access_token");
-    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-    const searchParams = new URLSearchParams(window.location.search);
-
-    const code = searchParams.get("code");
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
-      getEvents().then((events) => {
-        if (this.mounted) {
-          this.setState({ events, locations: extractLocations(events) });
-        }
-      });
-    }
-  }
-
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-
-  updateNumberOfEvents = (numberOfEvents) => {
-    this.setState({
-      numberOfEvents,
-    });
-
-    this.updateEvents(this.state.locations, numberOfEvents);
-  };
-
-  updateEvents = (location, eventCount) => {
-    getEvents().then((events) => {
-      const locationEvents =
-        location === "all"
-          ? events
-          : events.filter((event) => event.location === location);
-      if (this.mounted) {
-        this.setState({
-          events: locationEvents.slice(0, this.state.numberOfEvents),
+updateEvents = (location, eventCount) => {
+    const { currentLocation, numberOfEvents } = this.state;
+    if (location) {
+      getEvents().then((response) => {
+        const locationEvents =
+          location === "all"
+            ? response.events
+            : response.events.filter((event) => event.location === location);
+        const events = locationEvents.slice(0, numberOfEvents);
+        return this.setState({
+          events: events,
           currentLocation: location,
+        });
+      });
+    } else {
+      getEvents().then((response) => {
+        const locationEvents =
+          currentLocation === "all"
+            ? response.events
+            : response.events.filter(
+                (event) => event.location === currentLocation
+              );
+        const events = locationEvents.slice(0, eventCount);
+        return this.setState({
+          events: events,
           numberOfEvents: eventCount,
         });
-      }
-    });
-  };
+      });
+    }
+  }; 
 
   render() {
-    const { events } = this.state;
-    if (this.state.showWelcomeScreen === undefined)
-      return <div className="App" />;
+    const { locations, numberOfEvents, offlineText, events } = this.state;
     return (
       <div className="App">
-        {/* <StatusAlert text={this.state.infoText} /> */}
-        <CitySearch
-          locations={this.state.locations}
-          updateEvents={this.updateEvents}
-        />
+        <h1>Meet App</h1>
+        <h4>Choose your nearest city</h4>
+        <CitySearch updateEvents={this.updateEvents} locations={locations} />
+        <OfflineAlert text={offlineText} />
         <NumberOfEvents
-          numberOfEvents={this.state.numberOfEvents}
-          updateNumberOfEvents={this.updateNumberOfEvents}
+          updateEvents={this.updateEvents}
+          numberOfEvents={numberOfEvents}
         />
-        <WelcomeScreen
-          showWelcomeScreen={this.state.showWelcomeScreen}
-          getAccessToken={() => {
-            getAccessToken();
-          }}
-        />
-
-        <div className="data-vis-wrapper">
-          <EventGenre className= "charts" events={events}  />
-          <ResponsiveContainer  height={400}>
-            <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-              <CartesianGrid />
-              <XAxis type="category" dataKey="city" name="city" />
-              <YAxis
-                allowDecimals={false}
-                type="number"
-                dataKey="number"
-                name="number of events"
-              />
-              <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-              <Scatter data={this.getData()} fill="#8884d8" />
-            </ScatterChart>
-          </ResponsiveContainer>
-        </div>
-        <EventList className="event-container" events={this.state.events} />
+        <h4>Events in the next 7 days</h4>
+{/*         <ResponsiveContainer height={400}>
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+            <CartesianGrid />
+            <XAxis type="category" dataKey="date" name="date" />
+            <YAxis
+              allowDecimals={false}
+              type="number"
+              dataKey="number"
+              name="number of events"
+            />
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Scatter name="A school" data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer> */}
+        <EventList events={events} />
       </div>
     );
   }
