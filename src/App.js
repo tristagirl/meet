187b7be404a558a44123680d9bf363ec10e77335
Trista <tdahleen@me.com>
2022-04-18 15:41:58 -0500

    commit 4.8

--- src/App.js
+++ src/App.js
@@ -1,78 +1,130 @@
-import React from 'react';
-import './App.css';
-import './nprogress.css';
-import EventList from './EventList';
-import CitySearch from './CitySearch';
-import NumberOfEvents from './NumberOfEvents';
-import { extractLocations, getEvents } from './api';
+import React, { Component } from "react";
+import "./App.css";
+import EventList from "./EventList";
+import CitySearch from "./CitySearch";
+import NumberOfEvents from "./NumberOfEvents";
+import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
+import "./nprogress.css";
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+// import { StatusAlert } from "./Alert";
+import WelcomeScreen from "./WelcomeScreen";
 
-class App extends React.Component {
+
+class App extends Component {
   state = {
     events: [],
     locations: [],
-    numberOfEvents: '32',
-    location: 'all'
+    numberOfEvents: 32,
+    showWelcomeScreen: undefined,
   };
-  
-  updateNumberOfEvents = (eventCount) => {
-    this.setState({
-      numberOfEvents: eventCount
-    })
-    this.updateEvents(this.state.currentLocation, eventCount);
-  }
 
-  updateEvents = (location, eventCount) => {
-    getEvents().then((events => {
-      if (eventCount !== undefined) {
-        this.setState({
-          numberOfEvents: eventCount
-        })
-      }
-      // filter event list by location
-      let eventList = location !== 'all' ?
-        events.filter(event => event.location === location) :
-        events
-
-      // Shorten event list
-      let shortEventList = eventList.slice(0, this.state.numberOfEvents)
-
-      // Assign value to events state, assign currentLocation
-      this.setState({
-        events: shortEventList,
-        currentLocation: location
-      });
-    }));
-  }
-
-  componentDidMount() {
-    this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({
-          events,
-          locations: extractLocations(events)
-        });
-      }
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter(
+        (event) => event.location === location
+      ).length;
+      const city = location.split(", ").shift();
+      return { city, number };
     });
+    return data;
+  };
+
+  async componentDidMount() {
+    this.mounted = true;
+
+    const accessToken = localStorage.getItem("access_token");
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
   }
 
   componentWillUnmount() {
     this.mounted = false;
   }
 
+  updateNumberOfEvents = (numberOfEvents) => {
+    this.setState({
+      numberOfEvents,
+    });
+
+    this.updateEvents(this.state.locations, numberOfEvents);
+  };
+
+  updateEvents = (location, eventCount) => {
+    getEvents().then((events) => {
+      const locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
+      if (this.mounted) {
+        this.setState({
+          events: locationEvents.slice(0, this.state.numberOfEvents),
+          currentLocation: location,
+          numberOfEvents: eventCount,
+        });
+      }
+    });
+  };
 
   render() {
-    const { events, locations, numberOfEvents } = this.state;
+    const { events } = this.state;
+    if (this.state.showWelcomeScreen === undefined)
+      return <div className="App" />;
     return (
       <div className="App">
-        <div id="App__header">
-          <h1>Search for tech events</h1>
-          <p>This app uses the Google Calendar API in conjunction with a CareerFoundry calendar to fetch and filter events based on the city and number of events desired. Give it a try!</p>
+        {/* <StatusAlert text={this.state.infoText} /> */}
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
+        <NumberOfEvents
+          numberOfEvents={this.state.numberOfEvents}
+          updateNumberOfEvents={this.updateNumberOfEvents}
+        />
+        <WelcomeScreen
+          showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => {
+            getAccessToken();
+          }}
+        />
+
+        <div className="data-vis-wrapper">
+          <EventGenre className= "charts" events={events}  />
+          <ResponsiveContainer  height={400}>
+            <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+              <CartesianGrid />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis
+                allowDecimals={false}
+                type="number"
+                dataKey="number"
+                name="number of events"
+              />
+              <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
         </div>
-        <CitySearch locations={locations} numberOfEvents={numberOfEvents} updateEvents={this.updateEvents} />
-        <NumberOfEvents updateNumberOfEvents={number => { this.updateNumberOfEvents(number) }} />
-        <EventList events={events} numberOfEvents={numberOfEvents} />
-      </div >
+        <EventList className="event-container" events={this.state.events} />
+      </div>
     );
   }
 }
