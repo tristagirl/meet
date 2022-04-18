03a04cedd3febefbb351b65f0242890356488fec
Trista <tdahleen@me.com>
2022-04-18 15:41:42 -0500

    4.8

--- src/api.js
+++ src/api.js
@@ -2,6 +2,9 @@
 import { mockData } from "./mock-data";
 import axios from "axios";
 import NProgress from "nprogress";
+import WelcomeScreen from './WelcomeScreen';
+import { getEvents, extractLocations, checkToken, getAccessToken } from
+'./api';
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
